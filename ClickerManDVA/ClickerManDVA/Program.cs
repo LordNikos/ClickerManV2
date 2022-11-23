f1293468796078aef0a48bb96265f7281c16366b
lordnikos <lordnikos@ya.ru>
2022-11-23 08:39:08 +0300

    ***

---
+++ ClickerManDVA/ClickerManDVA/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using System.Runtime.InteropServices;
+
+namespace ClickerManDVA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (new System.Klava())
+                .VK_Q_Is(x => System.Console.WriteLine("true"), x => System.Console.WriteLine("false"))
+                .Sleep()
+                .VK_Q_Down()
+                .Sleep()
+                .VK_Q_Is(x => System.Console.WriteLine("true"), x => System.Console.WriteLine("false"))
+                .Sleep()
+                .VK_Q_Up()
+                .VK_Q_Is(x => System.Console.WriteLine("true"), x => System.Console.WriteLine("false"))
+            ;
+
+        }
+    }
+}
